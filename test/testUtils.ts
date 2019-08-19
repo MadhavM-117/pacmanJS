24fe4aef40c3462d511ffc5a4bd3c14c2c110188
Madhav Manoj <madhavmanoj117@gmail.com>
2019-08-19 21:26:44 +0530

    [levels] updated test functions for utils

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,6 +1,11 @@
 /* eslint-disable */
 import { expect } from 'chai';
-import { calculateSpeed } from '../src/utils';
+import {
+  calculateSpeed,
+  getLevelCoords,
+  getObjectBounds,
+  getObjectCorners
+} from '../src/utils';
 import 'mocha';
 import { describe, it } from 'mocha';
 import config from '../src/constants/config';
@@ -8,23 +13,53 @@ import config from '../src/constants/config';
 describe('Utils', () => {
   describe('calculateSpeed', () => {
     it('should calculate the speed given rotation angle 0', () => {
-      let expectedSpeed = { x: config.pacMan.defaultSpeed, y: 0.0 };
+      const expectedSpeed = { x: config.pacMan.defaultSpeed, y: 0.0 };
       expect(calculateSpeed(0)).to.eql(expectedSpeed);
     });
 
     it('should calculate the speed given rotation angle 90', () => {
-      let expectedSpeed = { x: 0.0, y: config.pacMan.defaultSpeed };
+      const expectedSpeed = { x: 0.0, y: config.pacMan.defaultSpeed };
       expect(calculateSpeed(90)).to.eql(expectedSpeed);
     });
 
     it('should calculate the speed given rotation angle 180', () => {
-      let expectedSpeed = { x: -config.pacMan.defaultSpeed, y: 0.0 };
+      const expectedSpeed = { x: -config.pacMan.defaultSpeed, y: 0.0 };
       expect(calculateSpeed(180)).to.eql(expectedSpeed);
     });
 
     it('should calculate the speed given rotation angle 270', () => {
-      let expectedSpeed = { x: 0.0, y: -config.pacMan.defaultSpeed };
+      const expectedSpeed = { x: 0.0, y: -config.pacMan.defaultSpeed };
       expect(calculateSpeed(270)).to.eql(expectedSpeed);
+    });
+  });
+
+  describe('getLevelCoords', () => {
+    it('should calculate level coordinates as 0, 0', () => {
+      const expectedCoords = { x: 0, y: 0 };
+      expect(getLevelCoords({ x: 10, y: 10 }, { x: 20, y: 20 })).to.eql(
+        expectedCoords
+      );
+    });
+
+    it('should calculate level coordinates as 1, 1', () => {
+      const expectedCoords = { x: 1, y: 1 };
+      expect(getLevelCoords({ x: 15, y: 15 }, { x: 10, y: 10 })).to.eql(
+        expectedCoords
+      );
+    });
+
+    it('should calculate level coordinates as 1, -1', () => {
+      const expectedCoords = { x: 1, y: -1 };
+      expect(getLevelCoords({ x: 15, y: -5 }, { x: 10, y: 10 })).to.eql(
+        expectedCoords
+      );
+    });
+
+    it('should calculate level coordinates as -1, 1', () => {
+      let expectedCoords = { x: -1, y: 1 };
+      expect(getLevelCoords({ x: -9, y: 18 }, { x: 10, y: 10 })).to.eql(
+        expectedCoords
+      );
     });
   });
 });
